8397aac624a312fd09c66d1cb50bf8d0b8a46b09
xHexed <58384565+xHexed@users.noreply.github.com>
2025-07-24 08:41:52 +0700

    fix: cors error

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -16,6 +16,7 @@ const axiosInstance: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
   },
 });
 
